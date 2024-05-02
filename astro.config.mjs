c2dab647335d5c9c227ea79f6b1d7bd15e0a3321
iVinci-KokiMiyawaki <kouki.miyawaki@i-vinci.co.jp>
2024-05-02 10:07:38 +0900

    レスポンシブ対応

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import db from '@astrojs/db'
-import { defineConfig } from 'astro/config'
+import db from '@astrojs/db';
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
-import react from '@astrojs/react'
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db(), react()],
-})
+  integrations: [db(), react(), icon()]
+});
